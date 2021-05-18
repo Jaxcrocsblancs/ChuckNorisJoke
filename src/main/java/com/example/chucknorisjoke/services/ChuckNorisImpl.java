79ea78f997329de2992e6c6ed5ab6dc929c43654
CALBA Antonin <jaxcrocsblancs@laposte.net>
2021-05-18 02:47:56 +0200

    Creating of service ChuckNorisImpl, Closes #3

--- src/main/java/com/example/chucknorisjoke/services/ChuckNorisImpl.java
+++ src/main/java/com/example/chucknorisjoke/services/ChuckNorisImpl.java
@@ -1,10 +1,17 @@
 package com.example.chucknorisjoke.services;
 
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
 public class ChuckNorisImpl implements ChuckNoris {
     private ChuckNorrisQuotes chuckNorrisQuotes;
 
+    public ChuckNorisImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    }
+
     @Override
     public String getJoke() {
-        return null;
+        return chuckNorrisQuotes.getRandomQuote();
     }
+
 }
